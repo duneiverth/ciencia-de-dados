eeb0b8fff2719fffc0473ab30f8556b8a1ec2dc8
Duda <105284664+duneiverth@users.noreply.github.com>
2024-10-24 08:31:48 -0300

    ok

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,5 +1,5 @@
 async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/duneiverth/ciencia-de-dados/refs/heads/main/base-de-dados/esportes-dados-globais.json'
+    const url = 'https://raw.githubusercontent.com/duneiverth/ciencia-de-dados/refs/heads/main/base-de-dados/esportes-mais-praticados.json'
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
